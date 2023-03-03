ae5b98549746538ec74115b7f7ee20be9638bd36
kemal <kemaltaufikf@gmail.com>
2023-03-03 17:40:03 +0800

    Add route chaining

--- index.js
+++ index.js
@@ -7,72 +7,74 @@ app.listen(PORT, () => {
   console.log(`The server is running on port ${PORT}`);
 });
 
-// get all data
-app.get("/students", (req, res) => {
-  fetch("http://localhost:4000/students")
-    .then((dataObj) => {
-      return dataObj.json();
+app
+  .route("/students")
+  .post((req, res) => {
+    const newPost = req.body;
+    newPost.id = shortid.generate();
+    fetch("http://localhost:4000/students", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(newPost),
     })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-// create data
-app.post("/students", (req, res) => {
-  const newPost = req.body;
-  newPost.id = shortid.generate();
-  fetch("http://localhost:4000/students", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(newPost),
+      .then((data) => {
+        res.send("data added");
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
   })
-    .then((data) => {
-      res.send("data added");
-    })
-    .catch((error) => {
-      console.error("Error:", error);
-    });
-});
-// get specific data
-app.get("/students/:id", (req, res) => {
-  const id = req.params.id;
-  fetch(`http://localhost:4000/students/${id}`)
-    .then((dataObj) => {
-      return dataObj.json();
-    })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
-// update data
-app.put("/students/:id", (req, res) => {
-  const id = req.params.id;
-  const updatedData = req.body;
-  fetch(`http://localhost:4000/students/${id}`, {
-    method: "PUT",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(updatedData),
+  .get((req, res) => {
+    fetch("http://localhost:4000/students")
+      .then((dataObj) => {
+        return dataObj.json();
+      })
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+app
+  .route("/students/:id")
+  .get((req, res) => {
+    const id = req.params.id;
+    fetch(`http://localhost:4000/students/${id}`)
+      .then((dataObj) => {
+        return dataObj.json();
+      })
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   })
-    .then((dataObj) => {
-      return dataObj.json();
+  .put((req, res) => {
+    const id = req.params.id;
+    const updatedData = req.body;
+    fetch(`http://localhost:4000/students/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(updatedData),
     })
-    .then((data) => {
-      res.send(data);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+      .then((dataObj) => {
+        return dataObj.json();
+      })
+      .then((data) => {
+        res.send(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
 app.use("/", (req, res) => {
   res.send("Ini home");
 });
