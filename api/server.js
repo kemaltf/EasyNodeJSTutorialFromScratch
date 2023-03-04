c1f73933bf523b41d03c10c25460b51bd16f2598
kemal <kemaltaufikf@gmail.com>
2023-03-05 00:59:05 +0800

    add several middleware helmet cors and morgan

--- api/server.js
+++ api/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import restricted from "../auth/restricted-middleware.js";
+import morgan from "morgan";
+import helmet from "helmet";
+import cors from "cors";
 dotenv.config();
 
 //setup routes
@@ -9,7 +12,9 @@ import usersRouter from "../Routes/user-routes.js";
 import authRouter from "../auth/auth-routes.js";
 
 const server = express();
-
+server.use(helmet());
+server.use(morgan("dev"));
+server.use(cors());
 server.use(express.json());
 
 server.use("/auth", authRouter);
